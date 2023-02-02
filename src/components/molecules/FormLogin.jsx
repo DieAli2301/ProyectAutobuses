e7deb5af5480651a38fc7d8d02c47467052df470
diegoOrtiz <diegoMP123@outlook.com>
2023-02-02 13:29:00 -0600

    [ADD] API en forms

--- src/components/molecules/FormLogin.jsx
+++ src/components/molecules/FormLogin.jsx
@@ -1,25 +1,63 @@
 import Header from "../atomos/Header.jsx";
 import { Link } from "react-router-dom";
 import "../../assets/Styles/FormLogin.css";
+import {useNavigate} from 'react-router-dom'
+import { useRef } from "react";
 
 function FormLogin() {
+    const navigate = useNavigate()
+
+    const form2 = useRef()
+    const handlerClick = (e) => {
+        e.preventDefault()
+        const formData = new FormData(form2.current);
+
+        let uri = 'http://34.225.239.102/api/iniciar';
+        let options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify({
+                usuario: formData.get('usuario'),
+                contrasenia: formData.get('contrasenia'),
+            
+            })
+        }
+        fetch(uri, options)
+        .then((response) => response.json())
+        .then((data) => {alert(JSON.stringify(data))});
+    
+        const newForm = new FormData(form2.current)
+        alert('  ' + newForm.get('usuario') + '  ' + newForm.get('contrasenia'));
+        navigate("/altaautobuses");
+    }
+
+   
+    
+
+
+
+
     return ( 
         <>
         
         <div id="divpadre">
+            <form ref={form2}>
         <Header />
             <div>
                 <div><label >Username</label></div>
-                <div><input type="text" /></div>
+                <div><input type="text" name="usuario"/></div>
             </div>
             <div>
                 <div><label htmlFor="">Password</label></div>
-                <div><input type="password" /></div>
+                <div><input type="password" name="contrasenia" /></div>
             </div>
             <div id="bton">
-                <div><button>Iniciar sesion</button></div>
+                <div><button onClick={handlerClick}>Iniciar sesion</button></div>
                 <div id="colorlink"><Link to="/register">No tienes cuenta? Registrate</Link></div>
             </div>
+            </form>
         </div>
         
         </>
--- src/components/molecules/FormRegister.jsx
+++ src/components/molecules/FormRegister.jsx
@@ -1,38 +1,84 @@
 import Header from "../atomos/Header";
 import '../../assets/Styles/FormRegister.css'
 import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function FormRegister() {
+    const navigate = useNavigate()
+
+    const FormR = useRef()
+    const handlerClick2 = (e) => {
+        e.preventDefault();
+
+    const formRegis= new FormData(FormR.current);
+
+        let URI="http://34.225.239.102/api/registrar/usuario"
+        let options = { //se crea un nuevo objeto
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            nombre: formRegis.get('nombre'),
+            usuario: formRegis.get('usuario'),
+            correo: formRegis.get('correo'),
+            contrasenia: formRegis.get('contrasenia')
+        })
+
+        
+    } 
+    
+    fetch(URI,options) //get
+    .then(response => response.json())
+    .then(data => {
+        alert(JSON.stringify(data))
+
+        const newFormRegister = new FormData(FormR.current)
+        alert(' Nombre: '+ newFormRegister.get('nombre') + '  UserName: '+ newFormRegister.get('usuario') + '  Correo: ' + newFormRegister.get('correo') + '  Password: ' + newFormRegister.get('contrasenia'));
+       
+        
+
+        navigate("/altaautobuses");
+    }
+    );
+
+}
+
+    
     return ( 
         <>
         
         <div id="divgran2">
+        <form ref={FormR}>
         <Header/>
             <div>
                 <div><label >Name</label></div>
-                <div><input type="text" /></div>
+                <div><input type="text" name="nombre" /></div>
             </div>
             <div>
                 <div><label htmlFor="">Email</label></div>
-                <div><input type="Email" /></div>
+                <div><input type="Email" name="correo"/></div>
             </div>
             <div>
                 <div><label htmlFor="">Username</label></div>
-                <div><input type="text" /></div>
+                <div><input type="text" name="usuario" /></div>
             </div>
             <div>
                 <div><label htmlFor="">Password</label></div>
-                <div><input type="password" /></div>
+                <div><input type="password" name="contrasenia" /></div>
             </div>
             <div id="bton2">
-                <div><button>Registro</button></div>
+                <div><button onClick={handlerClick2}>Registro</button></div>
                 <div><Link to="/altaautobuses">Registrar Autobus</Link></div>
             </div>
-            
+
+            </form>
         </div>
         </>
 
      );
 }
+
 
 export default FormRegister;
